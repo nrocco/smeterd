a7530d41dc5c6ad2071562df96e31c8891586483
Nico Di Rocco <dirocco.nico@gmail.com>
2013-05-05 20:41:49 +0200

    Removed bin/ folder (and moved bash rrd stuff to python)

--- smeterd/rrd.py
+++ smeterd/rrd.py
@@ -38,3 +38,34 @@ def import_data_into_rrd(database, rrd_file):
         rrdtool.update(rrd_file, '%s:%s:%s:%s' % 
                        (ts, row['kwh1'], row['kwh2'], row['gas']))
 
+
+
+
+#STEP=3600
+#PERIOD='--start end-7d --end now'
+#DIMENSIONS='--width 800 --height 300'
+#
+#RRD_FILE="test.rrd"
+#
+#[ -f $RRD_FILE ] || exit 1
+#
+#rrdtool graph kwh.png \
+#  --step $STEP $PERIOD $DIMENSIONS \
+#  --title 'Energy consumption in kWh' --vertical-label 'kWh' \
+#  --lower-limit 0 \
+#  --no-legend \
+#  DEF:kwh1=$RRD_FILE:kwh1:AVERAGE \
+#  DEF:kwh2=$RRD_FILE:kwh2:AVERAGE \
+#  CDEF:kwh1k=kwh1,1000,/ \
+#  CDEF:kwh2k=kwh2,1000,/ \
+#  AREA:kwh1k#0000FF:"Average kwh1" \
+#  AREA:kwh2k#00FF00:"Average kwh2"
+#
+#
+#rrdtool graph gas.png \
+#  --step $STEP $PERIOD $DIMENSIONS \
+#  --vertical-label 'Cubic meters' \
+#  --lower-limit 0 \
+#  --no-legend \
+#  DEF:gas=$RRD_FILE:gas:AVERAGE \
+#  AREA:gas#FF0000:"Average"
