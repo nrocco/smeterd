8138258424cc83762ff8959f75ba0e7d741db459
Nico Di Rocco <n.dirocco@global.leaseweb.com>
2016-11-14 20:39:38 +0100

    This will resolve #11 by allowing both S and W.

--- tests/test_meter.py
+++ tests/test_meter.py
@@ -74,7 +74,7 @@ def test_meter_read_one_packet():
 
     p = meter.read_one_packet()
     assert p['header'] == '/ISk5\2ME382-1004'
-    assert p['kwh']['eid'] == '4B414C37303035313135383130323132'
+    assert p['kwh']['eid'] == 'XXXXXXXXXXXXXXMYSERIALXXXXXXXXXXXXXX'
     assert p['kwh']['low']['consumed'] == 608.400
     assert p['kwh']['high']['consumed'] == 490.342
     assert p['kwh']['low']['produced'] == 0.001
