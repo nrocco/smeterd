91decd180e320419fe7a378c51db1cc3f0a9e71e
Nico Di Rocco <n.dirocco@global.leaseweb.com>
2016-11-28 22:50:45 +0100

    Add more cli options to configure the serial connection

--- smeterd/cli/read_meter.py
+++ smeterd/cli/read_meter.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import datetime
 
+import serial
 from serial.serialutil import SerialException
 from pycli_tools.commands import Command, arg
 
@@ -19,19 +20,23 @@ class ReadMeterCommand(Command):
     '''
 
     args = [
-        arg('--serial-port', default=__default_serial__, metavar=__default_serial__,
-            help='serial port to read packets from (defaults to %s)' % __default_serial__),
-        arg('--baudrate', default=9600,
-            help='baudrate for the serial connection'),
-        arg('--tsv', action='store_true',
-            help='display packet in tab seperated value form'),
-        arg('--raw', action='store_true',
-            help='display packet in raw form'),
+        arg('--serial-port', default=__default_serial__, metavar=__default_serial__, help='serial port to read packets from (defaults to %s)' % __default_serial__),
+        arg('--baudrate', default=9600, help='baudrate for the serial connection'),
+        arg('--xonxoff', action='store_true', help='wether to enable software flow control'),
+        arg('--bytesize', type=int, default=serial.SEVENBITS, help='byte size for the serial connection, choose from 5, 6, 7, 8'),
+        arg('--parity', default=serial.PARITY_EVEN, help='parity for the serial connection, choose from N, E, O, M, S'),
+        arg('--stopbits', type=float, default=serial.STOPBITS_ONE, help='stop bits for the serial connection, choose from 1, 1.5, 2'),
+        arg('--tsv', action='store_true', help='display packet in tab separated value form'),
+        arg('--raw', action='store_true', help='display packet in raw form'),
     ]
 
     def run(self, args, parser):
         meter = SmartMeter(args.serial_port,
-                           baudrate=args.baudrate)
+                           baudrate=args.baudrate,
+                           bytesize=args.bytesize,
+                           parity=args.parity,
+                           stopbits=args.stopbits,
+                           xonxoff=args.xonxoff)
 
         try:
             packet = meter.read_one_packet()
--- smeterd/meter.py
+++ smeterd/meter.py
@@ -15,10 +15,11 @@ class SmartMeter(object):
             self.serial = serial.Serial(
                 port,
                 kwargs.get('baudrate', 9600),
-                timeout=10,
-                bytesize=serial.SEVENBITS,
-                parity=serial.PARITY_EVEN,
-                stopbits=serial.STOPBITS_ONE
+                bytesize=kwargs.get('bytesize', serial.SEVENBITS),
+                parity=kwargs.get('parity', serial.PARITY_EVEN),
+                stopbits=kwargs.get('stopbits', serial.STOPBITS_ONE),
+                xonxoff=kwargs.get('xonxoff', False),
+                timeout=10
             )
         except (serial.SerialException,OSError) as e:
             raise SmartMeterError(e)
