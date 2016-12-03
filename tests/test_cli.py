3aa9a936db7f7fa625819e62133711cccc1e2798
Nico Di Rocco <n.dirocco@global.leaseweb.com>
2016-12-03 12:00:03 +0100

    Add unit tests for the read-meter subcommand

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,16 +1,53 @@
 import mock
-
 from smeterd.cli import parse_and_run
 
+
 @mock.patch('smeterd.cli.read_meter.SmartMeter')
-@mock.patch('smeterd.cli.read_meter.ReadMeterCommand')
-def test_cli_default_options(mocked_command, mocked_meter):
-    foo = parse_and_run(['read-meter', '--serial-port', '/foo/bar'])
+def test_cli_default_options(mocked_meter):
+    result = parse_and_run(['read-meter'])
 
-    print(mocked_command.run)
-    print(mocked_command.call_args)
-    print(mocked_command.run.call_count)
+    assert mocked_meter.call_args[0][0] == '/dev/ttyUSB0'
+    assert mocked_meter.call_args[1]['baudrate'] == 9600
+    assert mocked_meter.call_args[1]['bytesize'] == 7
+    assert mocked_meter.call_args[1]['parity'] == 'E'
+    assert mocked_meter.call_args[1]['stopbits'] == 1
+    assert mocked_meter.call_args[1]['timeout'] == 10
+    assert mocked_meter.call_args[1]['xonxoff'] == False
 
-    #assert mocked_command.run.call_args[0][0]['serial_port'] == '/foo/bar'
+    assert 0 == result
 
-    assert 0 == foo
+
+@mock.patch('smeterd.cli.read_meter.SmartMeter')
+def test_cli_custom_options(mocked_meter):
+    result = parse_and_run([
+        'read-meter',
+        '--serial-port', '/foo/bar',
+        '--serial-baudrate', '115200',
+        '--serial-bytesize', '8',
+        '--serial-parity', 'N',
+        '--serial-stopbits', '2',
+        '--serial-timeout', '5',
+        '--serial-xonxoff'
+    ])
+
+    assert mocked_meter.call_args[0][0] == '/foo/bar'
+    assert mocked_meter.call_args[1]['baudrate'] == 115200
+    assert mocked_meter.call_args[1]['bytesize'] == 8
+    assert mocked_meter.call_args[1]['parity'] == 'N'
+    assert mocked_meter.call_args[1]['stopbits'] == 2
+    assert mocked_meter.call_args[1]['timeout'] == 5
+    assert mocked_meter.call_args[1]['xonxoff'] == True
+
+    assert 0 == result
+
+
+@mock.patch('smeterd.cli.read_meter.SmartMeter')
+def test_cli_legacy_options(mocked_meter):
+    result = parse_and_run([
+        'read-meter',
+        '--baudrate', '115200',
+    ])
+
+    assert mocked_meter.call_args[1]['baudrate'] == 115200
+
+    assert 0 == result
